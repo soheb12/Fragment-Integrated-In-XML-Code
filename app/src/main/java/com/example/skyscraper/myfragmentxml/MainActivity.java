b97fd8a677570cd3cdf91aee5defbf8f3c957c3d
Shoeb Moin <Skyscraper@Venkats-MacBook-Pro.local>
2018-06-10 15:00:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/skyscraper/myfragmentxml/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.skyscraper.myfragmentxml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/skyscraper/myfragmentxml/Myfragment2.java
@@ -0,0 +1,19 @@
+package com.example.skyscraper.myfragmentxml;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Myfragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v;
+        v = inflater.inflate(R.layout.my_fragment_2,container,false);
+        return v;
+    }
+}
